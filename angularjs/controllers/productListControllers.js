7a9fa2931299d5e07e870a6dac9a085536498116
skotturi <skotturi@alumni.ucsd.edu>
2014-05-29 17:23:59 -0700

    adding pagination to controller

--- angularjs/controllers/productListControllers.js
+++ angularjs/controllers/productListControllers.js
@@ -1,11 +1,21 @@
 angular.module("sportsStore")
 	.constant("productListActiveClass", "btn-primary")
-	.controller("productListCtrl", function($scope, $filter, productListActiveClass){
+	.constant("productListPageCount", 3)
+	.controller("productListCtrl", function($scope, $filter, 
+		productListActiveClass, productListPageCount){
 
 		var selectedCategory = null; 
 
+		$scope.selectedPage = 1; 
+		$scope.pageSize = productListPageCount;
+
 		$scope.selectCategory = function(newCategory){
 			selectedCategory = newCategory;
+			$scope.selectedPage = 1; 
+		}
+
+		$scope.selectPage = function(newPage){
+			$scope.selectedPage = newPage; 
 		}
 
 		$scope.categoryFilterFn = function(product){
@@ -13,6 +23,10 @@ angular.module("sportsStore")
 		}
 
 		$scope.getCategoryClass = function(category){
-			return selectedCategory == category ? productListActiveClass: ""; 
+			return selectedCategory == category ? productListActiveClass : ""; 
+		}
+
+		$scope.getPageClass = function(page){
+			return $scope.selectedPage == page ? productListActiveClass : ""; 
 		}
 	});
