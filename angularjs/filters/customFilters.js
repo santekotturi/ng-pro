26f8e13d8de63d935cf451e5f3cd68e6513e9d4b
skotturi <skotturi@alumni.ucsd.edu>
2014-05-29 17:24:20 -0700

    adding pagination filter

--- angularjs/filters/customFilters.js
+++ angularjs/filters/customFilters.js
@@ -17,4 +17,39 @@ angular.module('customFilters', [])
 				}
 			
 		}
+	})
+	.filter("range", function($filter){
+		return function(data, page, size){
+			// console.log("data is", data);
+			// console.log("page is", page);
+			// console.log("size is", size);
+			if (angular.isArray(data) && angular.isNumber(page) && angular.isNumber(size)){
+				var start_index = (page - 1) * size; 
+				console.log("start index",start_index);
+				if (data.length > start_index){
+					// console.log("going to return nothing");
+					return [];
+				} else {
+					console.log("hooray");
+					return $filter("limitTo")(data.splice(start_index), size);
+				}
+			} else {
+				return data;
+			}
+		}
+	})
+	.filter("pageCount", function(){
+		return function(data, size){
+			if (angular.isArray(data)) {
+				var result = []; 
+				for (var i = 0; i < Math.ceil(data.length / size); i++) {
+					result.push(i);
+				}
+				// console.log("result is:", result);
+				return result;				
+			} else {
+				return data;
+			}
+		}
 	});
+	
