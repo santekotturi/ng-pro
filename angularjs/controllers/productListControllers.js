0df0b6a8ab50b00deecf7c12d13bbad9695118d8
skotturi <skotturi@ucsd.edu>
2014-06-03 17:56:53 -0700

    cleaning up homepage and controller

--- angularjs/controllers/productListControllers.js
+++ angularjs/controllers/productListControllers.js
@@ -1,33 +1,34 @@
 angular.module("sportsStore")
-	.constant("productListActiveClass", "btn-primary")
-	.constant("productListPageCount", 3)
-	.controller("productListCtrl", function($scope, $filter, 
-		productListActiveClass, productListPageCount){
+    .constant("productListActiveClass", "btn-primary")
+    .constant("productListPageCount", 3)
+    .controller("productListCtrl", function ($scope, $filter,
+        productListActiveClass, productListPageCount) {
 
-		var selectedCategory = null; 
+        var selectedCategory = null;
 
-		$scope.selectedPage = 1; 
-		$scope.pageSize = productListPageCount;
+        $scope.selectedPage = 1;
+        $scope.pageSize = productListPageCount;
 
-		$scope.selectCategory = function(newCategory){
-			selectedCategory = newCategory;
-			$scope.selectedPage = 1; 
-		}
+        $scope.selectCategory = function (newCategory) {
+            selectedCategory = newCategory;
+            $scope.selectedPage = 1;
+        }
 
-		$scope.selectPage = function(newPage){
-			$scope.selectedPage = newPage; 
-		}
+        $scope.selectPage = function (newPage) {
+            $scope.selectedPage = newPage;
+        }
 
-		$scope.categoryFilterFn = function(product){
-			return selectedCategory == null || 
-			product.category == selectedCategory; 
-		}
+        $scope.categoryFilterFn = function (product) {
+            return selectedCategory == null ||
+                product.category == selectedCategory;
+        }
 
-		$scope.getCategoryClass = function(category){
-			return selectedCategory == category ? productListActiveClass : ""; 
-		}
+        $scope.getCategoryClass = function (category) {
+            return selectedCategory == category ? productListActiveClass : "";
+        }
 
-		$scope.getPageClass = function(page){
-			return $scope.selectedPage == page ? productListActiveClass : ""; 
-		}
-	});
+        $scope.getPageClass = function (page) {
+            return $scope.selectedPage == page ? productListActiveClass : "";
+        }
+    });
+
